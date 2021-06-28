bf7f7729ea415d570f854e05574bc08fd0437a5e
tolumide-ng <tolumideshopein@gmail.com>
2021-06-28 17:47:27 +0100

    starts project with cargo run

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,36 @@
-use actix_cors::Cors;
 use actix_web::{get, http, web, App, HttpRequest, HttpResponse, HttpServer};
+use actix::prelude::{Addr, SyncArbiter};
 use std::env;
 
+// #[macro_use]
+// extern crate diesel;
+
+
+
+pub mod middlewares;
+pub mod db;
 
 #[get("/")]
 async fn index(req: HttpRequest) -> &'static str {
     "Hello World"
 }
 
+pub struct AppState {
+    // pub db: Addr<DbExecutor>,
+}
+
+
+
+#[actix_web::main]
 pub async fn start() -> std::io::Result<()> {
-    let client_url = env::var("FRONTEND_URL").ok();
+//     let client_url = env::var("FRONTEND_URL").ok();
+//     let database_url = env::var("DATABASE_URL").expect("PLEASE ADD DATABASE_URL");
+//     let database_address = SyncArbiter::start(num_cpus::get(), move || DbExec)
+   
 
-    HttpServer::new(|| {
-        // let cors = Cors::default().allow_any_origin().allow_any_method().allow_any_header().max_age(3600);
-        let cors = match client_url {
-            // Some(ref client) => Cors::new().allowed_origin()
-        }
 
-        App::new().wrap(cors).service(index)
-
-    }).bind(("127.0.0.1", 8000))?.run().await
+    HttpServer::new(|| App::new().service(index))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 use store;
 
 
-fn main() {    
+
+use actix_cors::Cors;
+use actix_web::{get, http, web, App, HttpRequest, HttpResponse, HttpServer};
+use std::env;
+
+
+fn main() {
+    if env::var("RUST_LOG").ok().is_none(){
+        env::set_var("RUST_LOG", "conduit=debug,actix_web=info");
+    }
+    
+
+
     store::start();
 }
+
+
+
